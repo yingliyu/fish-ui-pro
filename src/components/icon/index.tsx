718a703eeabba4a671defb94389a00bec9ecbe85
ylyu <yingliyu@aliyun.com>
2022-08-09 15:29:08 +0800

    test publish

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -2,30 +2,17 @@
  * @Author: ylyu
  * @Date: 2022-01-18 11:45:30
  * @LastEditors: ylyu
- * @LastEditTime: 2022-07-14 16:14:29
+ * @LastEditTime: 2022-08-09 15:28:41
  * @Description:
  */
-import {
-  HomeOutlined,
-  // LoadingOutlined,
-  // SettingFilled,
-  // SmileOutlined,
-  // SyncOutlined,
-} from '@ant-design/icons';
+import { HomeOutlined } from '@ant-design/icons';
 import { Space } from 'antd';
 import React from 'react';
 
 const Icon: React.FC<any> = ({ title }: { title: string }) => (
   <div>
     <h1>{title}</h1>
-    <Space>
-      {/* <HomeOutlined /> */}
-      {/* <SettingFilled />
-      <SmileOutlined />
-      <SyncOutlined spin />
-      <SmileOutlined rotate={180} />
-      <LoadingOutlined /> */}
-    </Space>
+    <Space>{/* <HomeOutlined /> */}</Space>
   </div>
 );
 
